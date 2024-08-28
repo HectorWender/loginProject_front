67101b4df22b5314f98b4ae14f9316af644aa80e
Hector Wender <hectorwender7@gmail.com>
2024-08-27 23:04:36 -0300

    FEAT - Melhorando layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  rightPanelActive = false;
+export class AppComponent implements OnInit {
+  rightPanelActive: boolean = false;
+  formE: FormGroup;
+
   @ViewChild('container', { static: false }) container: ElementRef | undefined;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.formE = this.formBuilder.group(this.populaFormEntrar());
+  }
 
   handleEntrarClick() {
     if (this.container)
@@ -23,10 +32,26 @@ export class AppComponent {
   }
 
   handleEntrarSubmit(event: Event) {
-    alert("Entrou")
+    alert('Entrou');
   }
 
   handleCadastrarSubmit(event: Event) {
     alert('Cadastrou');
+  }
+
+  private populaFormEntrar() {
+    return {
+      email: null,
+      senha: null,
+    };
+  }
+
+  private populaFormCadastrar() {
+    return {
+      user: null,
+      email: null,
+      senha: null,
+      confirmSenha: null,
+    };
   }
 }
